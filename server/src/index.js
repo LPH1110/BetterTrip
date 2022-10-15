ef5d8789d9ed53401bd0017d891e6f86b5b4527a
LPH1110 <lephuhaotech1124@gmail.com>
2022-10-15 09:09:02 +0700

    adding some lib + plugin

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// CORS
+app.use(cors());
 
 // HTTP loggers
 app.use(morgan("combined"));
@@ -10,4 +22,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to BetterTrip server!");
 });
 
-app.listen(3001);
+app.listen(port, () => {
+  console.log(`Server app listening on port ${port}`);
+});
