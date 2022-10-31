049431b1ebe068829775591c354e47a5fe052a9a
LPH1110 <lephuhaotech1124@gmail.com>
2022-10-31 12:31:50 +0700

    build Box ticket UI

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -10,6 +10,11 @@ const images = {
     envelopeIcon: require('./imgs/envelope.png'),
     phoneCallIcon: require('./imgs/phone-call.png'),
     markerIcon: require('./imgs/marker.png'),
+    userPng: require('./imgs/user-icon.png'),
+    planePng: require('./imgs/plane-icon.png'),
+    seatPng: require('./imgs/seat-icon.png'),
+    caretDownPng: require('./imgs/caret-down.png'),
+    tickPng: require('./imgs/tick-icon.png'),
 };
 
 export default images;
